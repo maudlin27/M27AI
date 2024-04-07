94888e0209c9cd3651107dc2e31c732134191ab9
maudlin27 <wormyworms10@hotmail.com>
2024-04-07 18:18:03 +0100

    Mod info

--- mod_info.lua
+++ mod_info.lua
@@ -1,8 +1,8 @@
 name = "M27AI"
-uid = "f27c55b4-v076-55b4-92b6-64398e75e23f"
-version = 76 --REMINDER: before release: Update M27Config; Version reference in uid updated; Tested offline vs AI it should beat to check for any last minute errors introduced
+uid = "f27c55b4-v077-55b4-92b6-64398e75e23f"
+version = 77 --REMINDER: before release: Update M27Config; Version reference in uid updated; Tested offline vs AI it should beat to check for any last minute errors introduced
 copyright = "GNU GENERAL PUBLIC LICENSE 3.0"
-description = "Adaptive AI, intended for 20km and smaller maps.  Est 700 1v1 ladder - 1200 global rating.  See the M27AI forum thread for more information.  Latest changes: Minor bugfixes, rating"
+description = "Adaptive AI, intended for 20km and smaller maps.  Est 700 1v1 ladder - 1200 global rating.  See the M27AI forum thread for more information.  Latest changes: Minor bugfixes"
 author = "maudlin27" --With thanks to Softles/Softlnoob for original Micro AI that was used as a starting point as I learned how to code AI
 --Also thanks to Softles for work scheduling code to help improve CPU performance
 url = "https://forum.faforever.com/topic/2373/ai-development-guide-and-m27ai-v1-devlog"
